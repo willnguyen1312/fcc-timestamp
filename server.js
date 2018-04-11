fc7a0288da570e2c6346bbb0154d90b4f22f9ef4
Nam Nguyen <willnguyen131292@outlook.com>
2018-04-11 12:24:38 +0700

    Add koa-compress

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
-const Koa = require('koa')
-const Router = require('koa-router')
-const Logger = require('koa-logger')
-const Cors = require('@koa/cors')
-const BodyParser = require('koa-bodyparser')
-const Helmet = require('koa-helmet')
-const respond = require('koa-respond')
+const Koa = require("koa");
+const Router = require("koa-router");
+const Logger = require("koa-logger");
+const Cors = require("@koa/cors");
+const BodyParser = require("koa-bodyparser");
+const Helmet = require("koa-helmet");
+const respond = require("koa-respond");
+const compress = require("koa-compress");
 
-const app = new Koa()
-const router = new Router()
+const app = new Koa();
+const router = new Router();
 
-app.use(Helmet())
+app.use(Helmet());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(Logger())
+if (process.env.NODE_ENV === "development") {
+  app.use(Logger());
 }
 
-app.use(Cors())
-app.use(BodyParser({
-  enableTypes: ['json'],
-  jsonLimit: '5mb',
-  strict: true,
-  onerror: function (err, ctx) {
-    ctx.throw('body parse error', 422)
-  }
-}))
+app.use(Cors());
+app.use(compress());
+app.use(
+  BodyParser({
+    enableTypes: ["json"],
+    jsonLimit: "5mb",
+    strict: true,
+    onerror: function(err, ctx) {
+      ctx.throw("body parse error", 422);
+    }
+  })
+);
 
-app.use(respond())
+app.use(respond());
 
 // API routes
-require('./routes')(router)
-app.use(router.routes())
-app.use(router.allowedMethods())
+require("./routes")(router);
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-module.exports = app
+module.exports = app;
