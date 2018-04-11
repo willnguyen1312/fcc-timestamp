b5ff8d7ad30389443b763d7988842c7aa5bde144
Nam Nguyen <willnguyen131292@outlook.com>
2018-04-11 12:28:21 +0700

    Fix linting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const server = require('./server')
+require("dotenv").config();
+const server = require("./server");
 
-const port = process.env.PORT || 3000
-server.listen(port, () => console.log(`API server started on ${port}`))
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`API server started on ${port}`));
